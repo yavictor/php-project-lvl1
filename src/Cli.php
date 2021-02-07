9e3e5a22487f2e040cf945256c212b5279d92670
yv <yavictor37@yandex.ru>
2021-02-07 17:07:15 +0300

    Added dependencies wp-cli/php-cli-tools, create first function for greeting player in src/, execute it in bin/brain-games through namespace.

---
+++ src/Cli.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+//require_once 'common.php';
+//
+//\cli\line('Welcome to the Brain Game!');
+//$name = \cli\prompt('May I have your name?');
+//\cli\line("Hello, %s!", $name);
