5c05cdb7702635e9efa73d7ee134081e043eb915
yv <yavictor37@yandex.ru>
2021-02-08 14:25:15 +0300

    Added even game

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/yavictor/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/yavictor/php-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
-![make lint](https://github.com/yavictor/php-project-lvl1/workflows/PHP%20Composer/badge.svg)
+[![make lint](https://github.com/yavictor/php-project-lvl1/workflows/PHP%20Composer/badge.svg)](https://github.com/yavictor/php-project-lvl1/actions?query=workflow%3A%22PHP+Composer%22)
---
+++ src/Even.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Brain\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+function isEven($number)
+{
+    return $number % 2 === 0;
+}
+
+function getRandom()
+{
+    return rand(1, 100);
+}
+
+//function askQuestion()
+//{
+//    $rightCounter = 0;
+//    for ($i = 0; $i < 3; $i++) {
+//        $random = getRandom();
+//        line("Question: ", $random);
+//        $rightAnswer = isEven($random) ? 'yes' : 'no';
+//        $answer = prompt('Your answer: ');
+//        if ($rightAnswer === $answer) {
+//            line('Correct!');
+//            $rightCounter ++;
+//        } else {
+//            line("{$answer} is wrong answer ;(. Correct answer was {$rightAnswer}.");
+//            return line("Let's try again, Bill!");
+//        }
+//    }
+//}
+
+function even()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $rightCounter = 0;
+    for ($i = 0; $i < 3; $i++) {
+        $random = getRandom();
+        line("Question: {$random}");
+        $rightAnswer = isEven($random) ? 'yes' : 'no';
+        $answer = prompt('Your answer: ');
+        if ($rightAnswer === $answer) {
+            line('Correct!');
+            $rightCounter ++;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+            return line("Let's try again, {$name}");
+        }
+    }
+    return line("Congratulations, {$name}!");
+}
